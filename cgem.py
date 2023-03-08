6fddfc4de2460eac5725d81bb01cbf371a871420
Lisa L. Lowe <lllowe@ncsu.edu>
2023-03-07 20:14:42 -0500

    added cgem python function

---
+++ cgem.py
@@ -0,0 +1,11 @@
+import subprocess
+import numpy as np
+import matplotlib as plt
+
+def cgem_plot1D(which_var):
+    print("Plotting CGEM variable",which_var)
+    results = subprocess.run(['./CGEM.exe',which_var],stdout=subprocess.PIPE, text=True)
+    ar = results.stdout.splitlines()
+    result = np.array(list(map(str.strip,ar))).astype(float)
+    plt.plot(result,linestyle='dotted')
+    plt.show()
